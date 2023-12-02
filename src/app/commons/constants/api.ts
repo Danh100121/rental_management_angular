2cda832cb44d7cb180a11f19be2e695f534cd1ea
Danh100121 <danh100121@gmail.com>
2023-12-02 23:12:33 +0700

    config api deploy

--- src/app/commons/constants/api.ts
+++ src/app/commons/constants/api.ts
@@ -1,1 +1,2 @@
-export const ROOT_API: string = "http://localhost:8080/api";
+// export const ROOT_API: string = "http://localhost:8080/api";
+export const ROOT_API: string = "https://rentalsystemapi-production.up.railway.app/api";
